4c206200d7c57a7e28a8b8d47dd23a98cca0dbe6
JoseCalvi1 <josecalvilloolmedo@gmail.com>
2021-08-03 18:23:25 +0200

    Sección sobre nosotros

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -50,4 +50,18 @@
                 @endforeach
         </div>
     </div>
+
+    <div class="container-fluid nuevas-recetas" style="background-color: #a7a4a4">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="titulo-categoria text-uppercase my-4 mx-5 text-white" style="border-bottom: 0px !important; font-weight:bold;">Sobre nosotros</h2>
+                <p class="my-4 mx-5 text-white" style="font-size: 1.2em; font-weight:bold;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a elit vel tortor condimentum imperdiet. Quisque sodales convallis nisl, at egestas sem finibus a. Sed venenatis mauris massa, et auctor orci lobortis quis.
+                    Curabitur consequat tempus blandit. Morbi id sem a magna dapibus consectetur. Nunc a eleifend neque, et placerat massa. Nulla ut porta dui, non sagittis diam. Q
+                    uisque suscipit enim in ipsum mattis, quis efficitur urna viverra. Vestibulum varius eget nulla egestas finibus. Morbi sodales vel nulla at eleifend. Quisque molestie euismod libero, id laoreet nunc dictum at.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="../images/sbbl.png" class="w-75">
+            </div>
+        </div>
+    </div>
 @endsection
