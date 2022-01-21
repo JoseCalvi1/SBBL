929295f6ec5b5ea7cdf792e1068fc2c0e780d599
JoseCalvi1 <josecalvilloolmedo@gmail.com>
2022-01-21 18:28:58 +0100

    Cambiar ranking Home

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -18,62 +18,66 @@
 
     <div class="container">
         <div class="row">
-    <div class="col-md-8 p-2">
-    <div class="row m-0">
-        <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Próximos eventos</h2>
+            <div class="col-md-12 p-2">
+            <div class="row m-0">
+                <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Próximos eventos</h2>
 
-        <div class="owl-carousel owl-theme">
-                @foreach ($nuevos as $nuevo)
-                        <div class="card">
-                            <img src="/storage/{{ $nuevo->imagen }}"  class="card-img-top">
+                <div class="owl-carousel owl-theme">
+                        @foreach ($nuevos as $nuevo)
+                                <div class="card">
+                                    <img src="/storage/{{ $nuevo->imagen }}"  class="card-img-top">
 
-                            <div class="card-body">
-                                <h3>{{ $nuevo->name }}</h3>
+                                    <div class="card-body">
+                                        <h3>{{ $nuevo->name }}</h3>
 
-                                <p>{{ $nuevo->location }} <b>({{ $nuevo->region->name }})</b></p>
-                                <p><event-date fecha="{{ $nuevo->date }}"></event-date></p>
+                                        <p>{{ $nuevo->location }} <b>({{ $nuevo->region->name }})</b></p>
+                                        <p><event-date fecha="{{ $nuevo->date }}"></event-date></p>
 
-                                <a href="{{ route('events.show', ['event' => $nuevo->id]) }}" class="btn btn-primary d-block font-weight-bold text-uppercase">Ver evento</a>
-                            </div>
-                        </div>
-                @endforeach
+                                        <a href="{{ route('events.show', ['event' => $nuevo->id]) }}" class="btn btn-primary d-block font-weight-bold text-uppercase">Ver evento</a>
+                                    </div>
+                                </div>
+                        @endforeach
+                </div>
+            </div>
+
+            <div class="row m-0">
+                <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Eventos realizados</h2>
+
+                <div class="owl-carousel owl-theme">
+                        @foreach ($antiguos as $antiguo)
+                                <div class="card">
+                                    <img src="/storage/{{ $antiguo->imagen }}"  class="card-img-top">
+
+                                    <div class="card-body">
+                                        <h3>{{ $antiguo->name }}</h3>
+
+                                        <p>{{ $antiguo->location }}</p>
+                                        <p><event-date fecha="{{ $antiguo->date }}"></event-date></p>
+
+                                        <a href="{{ route('events.show', ['event' => $antiguo->id]) }}" class="btn btn-primary d-block font-weight-bold text-uppercase">Ver evento</a>
+                                    </div>
+                                </div>
+                        @endforeach
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 nuevas-recetas">
+            <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Top 5 SBBL</h2>
+
+            <div class="col-md-2 text-center top-1"><h3>{{ $bladers[0]->user->name }}</h3></div>
+
+
+
+            <div style="width: 100%;color:rgb(196, 196, 2);border:2px solid rgb(196, 196, 2);"><span style="font-weight:bold;border-right:2px solid rgb(196, 196, 2);padding:2px 4px">1º</span><span style="padding: 2px 4px;">{{ $bladers[0]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[0]->points }} ptos</span></span></div>
+            <div style="width: 100%;color:rgb(71, 71, 70);border:2px solid rgb(71, 71, 70);"><span style="font-weight:bold;border-right:1px solid rgb(71, 71, 70);padding:2px 4px">2º</span><span style="padding: 2px 4px;">{{ $bladers[1]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[1]->points }} ptos</span></span></div>
+            <div style="width: 100%;color:rgb(126, 77, 3);border:2px solid rgb(126, 77, 3);"><span style="font-weight:bold;border-right:1px solid rgb(126, 77, 3);padding:2px 4px">3º</span><span style="padding: 2px 4px;">{{ $bladers[2]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[2]->points }} ptos</span></span></div>
+            <div style="width: 100%;color:rgb(36, 35, 35);border:2px solid rgb(36, 35, 35);"><span style="font-weight:bold;border-right:1px solid rgb(36, 35, 35);padding:2px 4px">4º</span><span style="padding: 2px 4px;">{{ $bladers[3]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[3]->points }} ptos</span></span></div>
+            <div style="width: 100%;color:rgb(36, 35, 35);border:2px solid rgb(36, 35, 35);"><span style="font-weight:bold;border-right:1px solid rgb(36, 35, 35);padding:2px 4px">5º</span><span style="padding: 2px 4px;">{{ $bladers[4]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[4]->points }} ptos</span></span></div>
+
+            <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Top Stamina</h2>
+            <div style="width: 100%;color:rgb(196, 196, 2);border:2px solid rgb(196, 196, 2);"><span style="font-weight:bold;border-right:2px solid rgb(196, 196, 2);padding:2px 4px">1º</span><span style="padding: 2px 4px;">{{ $stamina->user->name }} <span style="float: right;padding-right:2px;">TIEMPO</span></span></div>
         </div>
     </div>
-
-    <div class="row m-0">
-        <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Eventos realizados</h2>
-
-        <div class="owl-carousel owl-theme">
-                @foreach ($antiguos as $antiguo)
-                        <div class="card">
-                            <img src="/storage/{{ $antiguo->imagen }}"  class="card-img-top">
-
-                            <div class="card-body">
-                                <h3>{{ $antiguo->name }}</h3>
-
-                                <p>{{ $antiguo->location }}</p>
-                                <p><event-date fecha="{{ $antiguo->date }}"></event-date></p>
-
-                                <a href="{{ route('events.show', ['event' => $antiguo->id]) }}" class="btn btn-primary d-block font-weight-bold text-uppercase">Ver evento</a>
-                            </div>
-                        </div>
-                @endforeach
-        </div>
-    </div>
-</div>
-<div class="col-md-1"></div>
-<div class="col-md-3 nuevas-recetas">
-    <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Top 5 SBBL</h2>
-    <div style="width: 100%;color:rgb(196, 196, 2);border:2px solid rgb(196, 196, 2);"><span style="font-weight:bold;border-right:2px solid rgb(196, 196, 2);padding:2px 4px">1º</span><span style="padding: 2px 4px;">{{ $bladers[0]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[0]->points }} ptos</span></span></div>
-    <div style="width: 100%;color:rgb(71, 71, 70);border:2px solid rgb(71, 71, 70);"><span style="font-weight:bold;border-right:1px solid rgb(71, 71, 70);padding:2px 4px">2º</span><span style="padding: 2px 4px;">{{ $bladers[1]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[1]->points }} ptos</span></span></div>
-    <div style="width: 100%;color:rgb(126, 77, 3);border:2px solid rgb(126, 77, 3);"><span style="font-weight:bold;border-right:1px solid rgb(126, 77, 3);padding:2px 4px">3º</span><span style="padding: 2px 4px;">{{ $bladers[2]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[2]->points }} ptos</span></span></div>
-    <div style="width: 100%;color:rgb(36, 35, 35);border:2px solid rgb(36, 35, 35);"><span style="font-weight:bold;border-right:1px solid rgb(36, 35, 35);padding:2px 4px">4º</span><span style="padding: 2px 4px;">{{ $bladers[3]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[3]->points }} ptos</span></span></div>
-    <div style="width: 100%;color:rgb(36, 35, 35);border:2px solid rgb(36, 35, 35);"><span style="font-weight:bold;border-right:1px solid rgb(36, 35, 35);padding:2px 4px">5º</span><span style="padding: 2px 4px;">{{ $bladers[4]->user->name }} <span style="float: right;padding-right:2px;">{{ $bladers[4]->points }} ptos</span></span></div>
-
-    <h2 class="titulo-categoria text-uppercase mb-4 mt-4">Top Stamina</h2>
-    <div style="width: 100%;color:rgb(196, 196, 2);border:2px solid rgb(196, 196, 2);"><span style="font-weight:bold;border-right:2px solid rgb(196, 196, 2);padding:2px 4px">1º</span><span style="padding: 2px 4px;">{{ $stamina->user->name }} <span style="float: right;padding-right:2px;">TIEMPO</span></span></div>
-</div>
-</div>
 </div>
 
     <div class="container-fluid nuevas-recetas my-2 pb-2" style="background-color: #a7a4a4">
