429432943dac2c4508eae557352bd20fdaa3eb29
JoseCalvi1 <josecalvilloolmedo@gmail.com>
2024-06-14 11:19:02 +0200

    Base de datos de equipos

--- database/migrations/2024_05_13_155241_create_teams_table.php
+++ database/migrations/2024_05_13_155241_create_teams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateTeamsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('name');
             $table->text('description');
-            $table->string('article_type');
-            $table->string('custom_url')->nullable()->unique();
+            $table->integer('points_x1')->default(0);
             $table->longText('image')->nullable();
+            $table->longText('logo')->nullable();
+            $table->foreignId('captain_id')->references('id')->on('users');
+            $table->enum('status', ['pending', 'accepted'])->default('pending');
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('teams');
     }
 }
